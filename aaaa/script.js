cd3feb2dfa2896b7a2bd8e9d1f333bf6a911df8b
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-10-23 12:45:55 -0500

    Update script.js

--- aaaa/script.js
+++ aaaa/script.js
@@ -1,1 +1,17 @@
-
+$(function(){
+  $("#kwiklikor").click(function(){
+    $("#kltake").html("Take: $467");
+  });
+  $("#liquorize").click(function(){
+    $("#litake").html("Take: $220");
+  });
+  $("#centiliquor").click(function(){
+    $("#cltake").html("Take: $362");
+  });
+  $("#zooliquor").click(function(){
+    $("#zltake").html("Take: $154");
+  });
+  $("#uniliquor").click(function(){
+    $("#ultake").html("Take: $334");
+  });
+});
